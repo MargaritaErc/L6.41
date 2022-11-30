9160b3144a23f8db8739091e3c0d70cb5cdad473
m.rita1650@gmail.com <m.rita1650@gmail.com>
2022-11-30 16:47:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+int totalNegativeNumbers  = 0;
+bool b = true;
+while(b)
+{
+    Console.WriteLine("Хотите ввчести число? Если да наберите 1, если нет наберите 2, Ваш ответ:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n==1)
+    {
+        Console.Write("Введите число:");
+        int m= Convert.ToInt32(Console.ReadLine());
+        if (m>0)
+        {
+         totalNegativeNumbers = totalNegativeNumbers +1;
+       
+        }}
+    else
+    {
+        Console.Write($"Спасибо! Чисел больше введено {totalNegativeNumbers} раз");
+        b = false;
+    }
+}
